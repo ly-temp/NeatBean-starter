253da72a9c7cad54c2de6f99c52173ca198ae149
ly-temp <100673133+ly-temp@users.noreply.github.com>
2023-09-12 09:41:22 +0800

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update &&apt install -y p7zip-full
 mkdir -p /workspace/nb &&cd /workspace/nb
-wget https://download.oracle.com/java/20/latest/jdk-20_linux-x64_bin.tar.gz -O jdk.tar.gz
+wget https://download.oracle.com/java/20/latest/jdk-20_linux-x64_bin.tar.gz
 tar xf jdk-20_linux-x64_bin.tar.gz &&rm jdk-20_linux-x64_bin.tar.gz
 JAVA_HOME=$(realpath jdk-20.0.2)
 wget https://archive.apache.org/dist/netbeans/netbeans/18/netbeans-18-bin.zip
