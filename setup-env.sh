8d62be9657434cad2d77f9bf5f950859e26ea57a
ly-temp <100673133+ly-temp@users.noreply.github.com>
2023-09-12 09:47:46 +0800

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -6,5 +6,6 @@ tar xf jdk-20_linux-x64_bin.tar.gz &&rm jdk-20_linux-x64_bin.tar.gz
 JAVA_HOME=$(realpath jdk-20.0.2)
 wget https://archive.apache.org/dist/netbeans/netbeans/18/netbeans-18-bin.zip
 7z x netbeans-18-bin.zip &&rm netbeans-18-bin.zip
+chmod +x ./netbeans/bin/netbeans
 export DISPLAY=:0.0
 ./netbeans/bin/netbeans --jdkhome "$JAVA_HOME"
